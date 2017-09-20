3bc292320010186b51f84e22842b8e06dc2b7ea2
Jocelyn Le Sage <jocelyn@le-sage.com>
2017-09-20 07:15:54 -0400

    Keep original ownership of /etc/shadow.

--- rootfs/etc/cont-init.d/00-app-user-map.sh
+++ rootfs/etc/cont-init.d/00-app-user-map.sh
@@ -11,6 +11,8 @@ set -u # Treat unset variables as an error.
 cp /defaults/passwd /etc/passwd
 cp /defaults/group /etc/group
 cp /defaults/shadow /etc/shadow
+chown root:shadow /etc/shadow
+chmod 640 /etc/shadow
 
 echo "$APP_USER:x:$USER_ID:$GROUP_ID::/dev/null:/sbin/nologin" >> /etc/passwd
 echo "$APP_USER:x:$GROUP_ID:" >> /etc/group
