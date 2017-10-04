d23a643e8f6f68662c3b39084b38be07d9bb9d6f
Jocelyn Le Sage <jocelyn@le-sage.com>
2017-10-04 15:17:27 -0400

    Fixed path of services directory.

--- rootfs/etc/cont-init.d/00-set-app-deps.sh
+++ rootfs/etc/cont-init.d/00-set-app-deps.sh
@@ -8,13 +8,13 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
-find /etc/s6/services.d -mindepth 1 -maxdepth 1 -type d -not -name ".*" -not -name "s6-*" -not -name app -exec basename {} ';' | while read SVC
+find /etc/services.d -mindepth 1 -maxdepth 1 -type d -not -name ".*" -not -name "s6-*" -not -name app -exec basename {} ';' | while read SVC
 do
     # Avoid making circular dependency.
-    [ ! -f /etc/s6/services.d/$SVC/app.dep ] || continue
+    [ ! -f /etc/services.d/$SVC/app.dep ] || continue
 
     # Add dependency.
-    touch /etc/s6/services.d/app/$SVC.dep
+    touch /etc/services.d/app/$SVC.dep
 done
 
 # vim: set ft=sh :
