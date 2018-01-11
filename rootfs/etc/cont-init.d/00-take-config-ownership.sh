e186a8580ef0f62a5168d348dde525de131b93c1
Jocelyn Le Sage <jocelyn@le-sage.com>
2018-01-10 19:30:47 -0500

    Do not fail when taking ownership of /config fails but user has write access.

--- rootfs/etc/cont-init.d/00-take-config-ownership.sh
+++ rootfs/etc/cont-init.d/00-take-config-ownership.sh
@@ -7,8 +7,20 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
+log() {
+    echo "[cont-init.d] $(basename $0): $*"
+}
+
 if [ "${TAKE_CONFIG_OWNERSHIP:-1}" -eq 1 ]; then
-    chown -R $USER_ID:$GROUP_ID /config
+    if ! chown -R $USER_ID:$GROUP_ID /config; then
+        # Failed to take ownership of /config.  This could happen when,
+        # for example, the folder is mapped to a network share.
+        # Continue if we have write permission, else fail.
+        if s6-setuidgid $USER_ID:$GROUP_ID [ ! -w /config ]; then
+            log "ERROR: Failed to take ownership and no write permission on /config."
+            exit 1
+        fi
+    fi
 fi
 
 # vim: set ft=sh :
