5e2a38b72a9ddd91a07b209385fb6a5baf75cf72
Jocelyn Le Sage <jocelyn@le-sage.com>
2018-04-02 08:51:43 -0400

    Setup and define the XDG_RUNTIME_DIR directory.

---
+++ rootfs/etc/cont-init.d/00-xdg-runtime-dir.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/with-contenv sh
+
+#
+# Setup the XDG runtime directory.
+#
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -u # Treat unset variables as an error.
+
+rm -rf "$XDG_RUNTIME_DIR"
+mkdir -p "$XDG_RUNTIME_DIR"
+chown $USER_ID:$GROUP_ID "$XDG_RUNTIME_DIR"
+chmod 700 "$XDG_RUNTIME_DIR"
+
+# vim: set ft=sh :
