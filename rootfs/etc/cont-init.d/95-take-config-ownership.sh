8255a2d5df1889099a18db6dc20318b1255303a5
Jocelyn Le Sage <jocelyn@le-sage.com>
2018-09-17 07:48:35 -0400

    The init script now also takes ownership of config content.

--- rootfs/etc/cont-init.d/95-take-config-ownership.sh
+++ rootfs/etc/cont-init.d/95-take-config-ownership.sh
@@ -7,20 +7,18 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
-log() {
-    echo "[cont-init.d] $(basename $0): $*"
-}
-
 if [ "${TAKE_CONFIG_OWNERSHIP:-1}" -eq 1 ]; then
     if ! chown -R $USER_ID:$GROUP_ID /config; then
         # Failed to take ownership of /config.  This could happen when,
         # for example, the folder is mapped to a network share.
         # Continue if we have write permission, else fail.
-        if s6-setuidgid $USER_ID:$GROUP_ID [ ! -w /config ]; then
-            log "ERROR: Failed to take ownership and no write permission on /config."
+        if su-exec $USER_ID [ ! -w /config ]; then
+            echo "ERROR: Failed to take ownership and no write permission on /config."
             exit 1
         fi
     fi
+
+    find /config -mindepth 1 -exec chown $USER_ID:$GROUP_ID {} \;
 fi
 
 # vim:ft=sh:ts=4:sw=4:et:sts=4
