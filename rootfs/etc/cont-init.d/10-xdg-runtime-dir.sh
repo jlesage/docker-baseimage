129252af46dc80fc91e3d3bcaa738b09b94b5f0f
Jocelyn Le Sage <jocelyn@le-sage.com>
2022-04-14 09:19:53 -0400

    Added comment to provide more details about the XDG runtime directory.

--- rootfs/etc/cont-init.d/10-xdg-runtime-dir.sh
+++ rootfs/etc/cont-init.d/10-xdg-runtime-dir.sh
@@ -3,6 +3,14 @@
 #
 # Setup the XDG runtime directory.
 #
+# According to https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html:
+#
+# $XDG_RUNTIME_DIR defines the base directory relative to which user-specific
+# non-essential runtime files and other file objects (such as sockets, named
+# pipes, ...) should be stored. The directory MUST be owned by the user, and he
+# MUST be the only one having read and write access to it. Its Unix access mode
+# MUST be 0700.
+#
 
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
