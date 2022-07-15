a61b076f7ab219817916c769dfae1ba02916cc07
Jocelyn Le Sage <jocelyn@le-sage.com>
2022-07-14 20:35:27 -0400

    Make the Docker image information display larger.

--- rootfs/etc/cont-init.d/89-info.sh
+++ rootfs/etc/cont-init.d/89-info.sh
@@ -7,12 +7,12 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
-printf "╭―――――――――――――――――――――――――――――――――――――――――――――――――――╮\n"
-printf "│                                                   │\n"
-printf "│ Application:          %-27s │\n" "$(echo "$APP_NAME" | sed 's/\(.\{24\}\).*/\1.../')"
-printf "│ Application Version:  %-27s │\n" "${APP_VERSION:-n/a}"
-printf "│ Docker Image Version: %-27s │\n" "${DOCKER_IMAGE_VERSION:-n/a}"
-printf "│                                                   │\n"
-printf "╰―――――――――――――――――――――――――――――――――――――――――――――――――――╯\n"
+printf ":::    ╭――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――╮\n"
+printf ":::    │                                                                      │\n"
+printf ":::    │ Application:          %-46s │\n" "$(echo "$APP_NAME" | sed 's/\(.\{24\}\).*/\1.../')"
+printf ":::    │ Application Version:  %-46s │\n" "${APP_VERSION:-n/a}"
+printf ":::    │ Docker Image Version: %-46s │\n" "${DOCKER_IMAGE_VERSION:-n/a}"
+printf ":::    │                                                                      │\n"
+printf ":::    ╰――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――╯\n"
 
 # vim:ft=sh:ts=4:sw=4:et:sts=4
