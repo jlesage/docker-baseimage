9433d6d97ddd39964aca713ec3f36cd98b6ebc21
Jocelyn Le Sage <jocelyn@le-sage.com>
2022-10-10 09:36:51 -0400

    Fixed taking ownership of /config when its subdirectories are mapped to a network share.

--- rootfs/etc/cont-init.d/85-take-config-ownership.sh
+++ rootfs/etc/cont-init.d/85-take-config-ownership.sh
@@ -12,20 +12,33 @@ if [ "${TAKE_CONFIG_OWNERSHIP:-1}" -eq 0 ]; then
     exit 0
 fi
 
-# Take ownership of /config.
-if ! chown $USER_ID:$GROUP_ID /config; then
-    # Failed to take ownership of /config.  This could happen when,
-    # for example, the folder is mapped to a network share.
-    # Continue if we have write permission, else fail.
-    if su-exec $USER_ID [ ! -w /config ]; then
-        echo "ERROR: Failed to take ownership and no write permission on /config."
-        exit 1
-    fi
-fi
-
-# Take ownership of all files and folders under /config.
+# Take ownership of /config.  Special care is done on /config and its children
+# directories, because these could be mapped to a network share.
 if [ -n "$(ls -A /config)" ]; then
-    find /config -maxdepth 1 -mindepth 1 -print0 | xargs -0 chown -R $USER_ID:$GROUP_ID
+    for DIR in $(find /config -maxdepth 1 -type d)
+    do
+        if ! chown $USER_ID:$GROUP_ID $DIR 2>/dev/null
+        then
+            # Failed to take ownership of directory.  This could happen when, for
+            # example, the folder is mapped to a network share.  Continue if we
+            # have write permission, else fail.
+            TMPFILE="$(su-exec $USER_ID mktemp "$DIR"/.test_XXXXXX 2>/dev/null)"
+            if [ $? -eq 0 ]; then
+              # Success, we were able to write file.
+                su-exec $USER_ID rm "$TMPFILE"
+            else
+                echo "ERROR: Failed to take ownership and no write permission on $DIR."
+                exit 1
+            fi
+        fi
+
+        # Take ownership of files and directories under the current directory.
+        if [ "$DIR" = "/config" ]; then
+            find $DIR -maxdepth 1 -mindepth 1 -type f -print0 | xargs -0 chown $USER_ID:$GROUP_ID
+        else
+            find $DIR -maxdepth 1 -mindepth 1 -print0 | xargs -0 chown -R $USER_ID:$GROUP_ID
+        fi
+    done
 fi
 
 # vim:ft=sh:ts=4:sw=4:et:sts=4
