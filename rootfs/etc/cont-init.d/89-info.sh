347227d3c66d2483dc79cf4bc85f308c0c86d577
Jocelyn Le Sage <jocelyn@le-sage.com>
2022-10-28 18:13:38 -0400

    Fixed length at which application name is truncated.

--- rootfs/etc/cont-init.d/89-info.sh
+++ rootfs/etc/cont-init.d/89-info.sh
@@ -9,7 +9,7 @@ set -u # Treat unset variables as an error.
 
 printf ":::    ╭――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――╮\n"
 printf ":::    │                                                                      │\n"
-printf ":::    │ Application:           %-45s │\n" "$(echo "$APP_NAME" | sed 's/\(.\{24\}\).*/\1.../')"
+printf ":::    │ Application:           %-45s │\n" "$(echo "$APP_NAME" | sed 's/\(.\{42\}\).*/\1.../')"
 printf ":::    │ Application Version:   %-45s │\n" "${APP_VERSION:-n/a}"
 printf ":::    │ Docker Image Version:  %-45s │\n" "${DOCKER_IMAGE_VERSION:-n/a}"
 printf ":::    │ Docker Image Platform: %-45s │\n" "${DOCKER_IMAGE_PLATFORM:-unknown}"
