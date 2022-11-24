8695c2616b9e6d0289e911dbe85d5dada6462ae2
Jocelyn Le Sage <jocelyn@le-sage.com>
2022-11-23 20:32:26 -0500

    Added helper to take ownership of a directory.

--- rootfs/etc/cont-init.d/85-take-config-ownership.sh
+++ rootfs/etc/cont-init.d/85-take-config-ownership.sh
@@ -12,33 +12,6 @@ if is-bool-val-false "${TAKE_CONFIG_OWNERSHIP:-1}"; then
     exit 0
 fi
 
-# Take ownership of /config.  Special care is done on /config and its children
-# directories, because these could be mapped to a network share.
-if [ -n "$(ls -A /config)" ]; then
-    for DIR in $(find /config -maxdepth 1 -type d)
-    do
-        if ! chown $USER_ID:$GROUP_ID $DIR 2>/dev/null
-        then
-            # Failed to take ownership of directory.  This could happen when, for
-            # example, the folder is mapped to a network share.  Continue if we
-            # have write permission, else fail.
-            TMPFILE="$(su-exec $USER_ID mktemp "$DIR"/.test_XXXXXX 2>/dev/null)"
-            if [ $? -eq 0 ]; then
-              # Success, we were able to write file.
-                su-exec $USER_ID rm "$TMPFILE"
-            else
-                echo "ERROR: Failed to take ownership and no write permission on $DIR."
-                exit 1
-            fi
-        fi
-
-        # Take ownership of files and directories under the current directory.
-        if [ "$DIR" = "/config" ]; then
-            find $DIR -maxdepth 1 -mindepth 1 -type f -print0 | xargs --no-run-if-empty -0 chown $USER_ID:$GROUP_ID
-        else
-            find $DIR -maxdepth 1 -mindepth 1 -print0 | xargs --no-run-if-empty -0 chown -R $USER_ID:$GROUP_ID
-        fi
-    done
-fi
+take-ownership /config
 
 # vim:ft=sh:ts=4:sw=4:et:sts=4
