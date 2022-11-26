8f580995880412001278cf97afa6d1d1161d8901
Jocelyn Le Sage <jocelyn@le-sage.com>
2022-11-25 19:30:39 -0500

    Added init script to set permissions on /tmp.

---
+++ rootfs/etc/cont-init.d/10-set-tmp-dir-perms.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#
+# Make sure permissions on the /tmp directory are properly set.
+#
+# We have seen cases on QNAP NAS where, for some reason, permissions were
+# incorrect.  See https://github.com/jlesage/docker-jdownloader-2/issues/98.
+#
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -u # Treat unset variables as an error.
+
+chmod 1777 /tmp
+
+# vim:ft=sh:ts=4:sw=4:et:sts=4
